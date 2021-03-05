871c3d93b0c4c20d0b4a01eb83d3c687e27e3ca3
BryanBernal <bryanbernal93@gmail.com>
2021-03-04 21:23:26 -0500

    initTest

---
+++ src/addProducts.js
@@ -0,0 +1,48 @@
+const coTest = require('./coTest');
+const Product = coTest.Product;
+const CarInsurance = coTest.CarInsurance;
+
+const arrProducts = [
+  [
+    'Medium Coverage',
+    10,
+    20
+  ],
+  [
+    'Coverage',
+    10,
+    20
+  ]
+]
+let name = null
+for (let i = 0; i < arrProducts.length; i++) {
+  name = arrProducts[0][0];
+  
+  
+}
+console.log(name);
+
+//return 1;
+const productsAtDayZero = [
+    new Product('Medium Coverage', 10, 20),
+    new Product('Full Coverage', 2, 0),
+    new Product('Low Coverage', 5, 7),
+    new Product('Mega Coverage', 0, 80),
+    new Product('Mega Coverage', -1, 80),
+    new Product('Special Full Coverage', 15, 20),
+    new Product('Special Full Coverage', 10, 49),
+    new Product('Special Full Coverage', 5, 49),
+    new Product('Super Sale', 3, 6),
+  ];
+  
+  const carInsurance = new CarInsurance(productsAtDayZero);
+  const productPrinter = function (product) {
+    console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
+  };
+  
+  for (let i = 1; i <= 30; i += 1) {
+    console.log(`Day ${i}`);
+    console.log('name, sellIn, price');
+    carInsurance.updatePrice().forEach(productPrinter);
+    console.log('');
+  }
